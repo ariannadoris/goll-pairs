adc00191838bb73d5c7b087187bfd09a4645ca4e
nelburg <elburgnw@gmail.com>
2020-06-25 02:04:21 +0200

    filter metadata on object we actually used

---
+++ metadata/metadata.js
@@ -0,0 +1,344 @@
+var metadataInformation = [
+{
+    "ID": "000009",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4036-1 à 4036-4",
+    "CLASSIFICATION": "VIIIA8 (1) à (4)",
+    "DESCRIPTION": "Photos de Claire Goll: 1. à 16 ans; 2. Au lac de Starnberg à 18 ans en 1909; 3. à Munich en 1909; 4. le 21/12/1911, jour de son mariage avec H Studer /Photographs of Claire Goll at  a young age (16 years old, 18 years old at the Stanberg lake in 1909, in 1909 in Munich and her wedding’s day with H. Studer, December the 21th, 1911)",
+    "MATERIALS": "Reproductions noir et blanc, papier glacé",
+    "DIMENSIONS": "1: 10,3X5,4 cm / 2: 10,9X8,1 cm / 3: 11,3X10,6 cm / 4: 9X8,3 cm"
+}
+,
+{
+    "ID": "000141",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "1980-297",
+    "CLASSIFICATION": "VIIIE142 (1) (2) et (3)",
+    "DESCRIPTION": "Malaiische Liebeslieder",
+    "MATERIALS": "Livres brochés (1) jaquette blanche collée / (2) jaquette brune amovible",
+    "DIMENSIONS": "(1) 0,209X0,133 m / (2) 0,21X0,134 m",
+    "AUTHOR": "Ivan Goll"
+}
+,
+{
+    "ID": "000171",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2004-22",
+    "CLASSIFICATION": "VIIIE172",
+    "DESCRIPTION": "Europe. Revue littéraire mensuelle (Yvan Goll)",
+    "MATERIALS": "Livre broché",
+    "DIMENSIONS": "0,217X0,13 m",
+    "AUTHOR": "Jean Bertho, Lionel Richard, Eric Robertson, Joseph Delteil, Barbara Glavert-Hesse, Ljubomir Micic, Marc Martin, Catherine Wermester, Albert Ronsin, Alain Virmaux, Henri Béhar, Philippe Brun, Joëlle Gardes Tamine, Pierre Ricas, Danier Grandidier"
+}
+,
+{
+    "ID": "000170",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2003-45",
+    "CLASSIFICATION": "VIIIE171",
+    "DESCRIPTION": "TTOIMATA (1920-1950) Poèmes choisis",
+    "MATERIALS": "Livre broché (feuilles à découper) 310 pages",
+    "DIMENSIONS": "0,25X0,18 m",
+    "AUTHOR": "Yvan Goll, traduction en grec par E. X. GONATAS"
+}
+,
+{
+    "ID": "000170",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2003-45",
+    "CLASSIFICATION": "VIIIE171",
+    "DESCRIPTION": "TTOIMATA (1920-1950) Poèmes choisis",
+    "MATERIALS": "Livre broché (feuilles à découper) 310 pages",
+    "DIMENSIONS": "0,25X0,18 m",
+    "AUTHOR": "Yvan Goll, traduction en grec par E. X. GONATAS"
+}
+,
+{
+    "ID": "000170",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2003-45",
+    "CLASSIFICATION": "VIIIE171",
+    "DESCRIPTION": "TTOIMATA (1920-1950) Poèmes choisis",
+    "MATERIALS": "Livre broché (feuilles à découper) 310 pages",
+    "DIMENSIONS": "0,25X0,18 m",
+    "AUTHOR": "Yvan Goll, traduction en grec par E. X. GONATAS"
+}
+,
+{
+    "ID": "000004",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4031-1 à 4031-8",
+    "CLASSIFICATION": "VIIIA3 (1) à (8)",
+    "DESCRIPTION": "Photo d'Yvan Goll entre 1919 et 1928: 1. 1919 (2 photos); 2. 1920; 3.1921; 4. 1921 (2 photos); 5. 1922 à 1924 (2 photos); 6. 1924 en pied (2 ex); 7. 1927 (2 photos); 8. 1928 (4 photos) /Photographs of Yvan Goll between 1919 and 1928",
+    "MATERIALS": "Photos noir et blanc, papier glacé / Photos in black and withe, glossy photographic paper",
+    "AUTHOR": "E.H. MARTINE"
+}
+,
+{
+    "ID": "000122",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4769",
+    "CLASSIFICATION": "VIIIE122 (a, b)",
+    "DESCRIPTION": "Die Antirose",
+    "MATERIALS": "Livre broché, couverture illustrée",
+    "DIMENSIONS": "19,7X12 cm",
+    "AUTHOR": "Ivan und Claire Goll"
+}
+,
+{
+    "ID": "000122",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4769",
+    "CLASSIFICATION": "VIIIE122 (a, b)",
+    "DESCRIPTION": "Die Antirose",
+    "MATERIALS": "Livre broché, couverture illustrée",
+    "DIMENSIONS": "19,7X12 cm",
+    "AUTHOR": "Ivan und Claire Goll"
+}
+,
+{
+    "ID": "000033",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4060",
+    "CLASSIFICATION": "VIIIA32 (Négatif voir A26)",
+    "DESCRIPTION": "Reproduction photo d'un autoportrait de Robert DELAUNAY /Photo reproduction of a Robert Delaunay’s portrait",
+    "MATERIALS": "Photo noir et blanc, papier mat / original: Huil sur toile",
+    "DIMENSIONS": "Photo: 239X179 mm / original: 550X460 mm",
+    "AUTHOR": "Original: Robert DELAUNAY / Reproduction réalisée d'après une photo de Marc VAUX (Paris)"
+}
+,
+{
+    "ID": "000034",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4061",
+    "CLASSIFICATION": "VIIIA33 (1) et (2) (négatifs)",
+    "DESCRIPTION": "1: 9 reproductions photos du dessin de CHAGALL \"Yvan Goll profil droit\" / 2: négatif  plan film /N. 1 : Nine photo reproductions of Chagall’s drawing : « Yvan Goll right profile » / N. 2 : Negative film",
+    "MATERIALS": "1: Photos noir et blanc, papier glacé (6 ex.), 3 reproductions sur line / 2: Négatif plan film / Original; dessin à l'encre",
+    "AUTHOR": "Original: Dessin CHAGALL"
+}
+,
+{
+    "ID": "000170",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2003-45",
+    "CLASSIFICATION": "VIIIE171",
+    "DESCRIPTION": "TTOIMATA (1920-1950) Poèmes choisis",
+    "MATERIALS": "Livre broché (feuilles à découper) 310 pages",
+    "DIMENSIONS": "0,25X0,18 m",
+    "AUTHOR": "Yvan Goll, traduction en grec par E. X. GONATAS"
+}
+,
+{
+    "ID": "000010",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4037-1 à 4037-6",
+    "CLASSIFICATION": "VIIIA9 (1) à (6)",
+    "DESCRIPTION": "Portraits de Claire GOLL  1918-1927: 1. 1918 à Ascona; 2 et 3. 1921 à Paris; 4. Berlin 1924; 5. Paris 1924; 6. 1927 Robe de Sonia DELAUNAY /Portraits of Claire Goll between 1918 and 1927  in : Ascona, Paris, Berlin, and the Sonia’s Delaunay dress in 1927",
+    "MATERIALS": "1: un ex. en papier glacé et 1 ex. papier mat; 2 et 3: papier glacé noir et blanc; 4: Papier glacé noir et blanc et negatif; 5: Papier glacé noir et blanc et negatif; 6: Papier glacé noir et blanc et une reproduction sur line",
+    "DIMENSIONS": "1: 16,8X10,9 cm / 2: 22,3X16,6 cm / 3: 22,3X16,6 cm / 4: 24X 17,8 cm / 5: 16,2X12,6 cm / 20,8X 16,2 cm",
+    "AUTHOR": "2 et 3: E. H. MARTINE / Riess / 5 et 6 R. Sobol - Paris"
+}
+,
+{
+    "ID": "000170",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2003-45",
+    "CLASSIFICATION": "VIIIE171",
+    "DESCRIPTION": "TTOIMATA (1920-1950) Poèmes choisis",
+    "MATERIALS": "Livre broché (feuilles à découper) 310 pages",
+    "DIMENSIONS": "0,25X0,18 m",
+    "AUTHOR": "Yvan Goll, traduction en grec par E. X. GONATAS"
+}
+,
+{
+    "ID": "000133",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4780",
+    "CLASSIFICATION": "VIIIE133",
+    "DESCRIPTION": "Claire Goll",
+    "MATERIALS": "Livre broché",
+    "DIMENSIONS": "16,1X13,4 cm",
+    "AUTHOR": "Georges Cattaui, Edmée De La Rochefoucauld, Armand Lanoux"
+}
+,
+{
+    "ID": "000130",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4777",
+    "CLASSIFICATION": "VIIIE130 (a,b)",
+    "DESCRIPTION": "Yvan Goll (2ex.)",
+    "MATERIALS": "Livre broché",
+    "DIMENSIONS": "15,8X13,5 cm",
+    "AUTHOR": "Jules Romains, Francis Carmody, Moral Brion, Richard Exner"
+}
+,
+{
+    "ID": "000170",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2003-45",
+    "CLASSIFICATION": "VIIIE171",
+    "DESCRIPTION": "TTOIMATA (1920-1950) Poèmes choisis",
+    "MATERIALS": "Livre broché (feuilles à découper) 310 pages",
+    "DIMENSIONS": "0,25X0,18 m",
+    "AUTHOR": "Yvan Goll, traduction en grec par E. X. GONATAS"
+}
+,
+{
+    "ID": "000161",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "1985-86",
+    "CLASSIFICATION": "VIIIE162",
+    "DESCRIPTION": "Sodom Berlin par Yvan Goll",
+    "MATERIALS": "Livre couverture cartonnée",
+    "DIMENSIONS": "0,21X0,12X0,016m",
+    "AUTHOR": "Goll Yvan, traduit du français par Hans THILL postface par Karten WITTE"
+}
+,
+{
+    "ID": "000172",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2004-23",
+    "CLASSIFICATION": "VIIIE173",
+    "DESCRIPTION": "Surréalisme N. 1",
+    "MATERIALS": "Livre relié",
+    "AUTHOR": "Ivan Goll, textes de Guillaume Apollinaire, Marcel Araland, Pierre Albert-Birot, René Crevel, Joseph Delteil, Robert Delaunay, Paul Demee, Jean Painlevé, Pierre Reverdy, Texte de Jean Bertho"
+}
+,
+{
+    "ID": "000154",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "I",
+    "INVENTORY": "1995-35",
+    "CLASSIFICATION": "VIIIC154",
+    "DESCRIPTION": "Femme au vase",
+    "MATERIALS": "Dessin à l'encre de chine (au pinceau), gouache",
+    "DIMENSIONS": "0,32X0,25 m",
+    "AUTHOR": "Fernand LEGER"
+}
+,
+{
+    "ID": "000133",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4780",
+    "CLASSIFICATION": "VIIIE133",
+    "DESCRIPTION": "Claire Goll",
+    "MATERIALS": "Livre broché",
+    "DIMENSIONS": "16,1X13,4 cm",
+    "AUTHOR": "Georges Cattaui, Edmée De La Rochefoucauld, Armand Lanoux"
+}
+,
+{
+    "ID": "000144",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4790",
+    "CLASSIFICATION": "VIIIE145",
+    "DESCRIPTION": "Le Beau navire. Revue de la poèsie",
+    "MATERIALS": "Livre broché",
+    "DIMENSIONS": "0,192X0,142X0,006  m"
+}
+,
+{
+    "ID": "000134",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4781",
+    "CLASSIFICATION": "VIIIE134",
+    "DESCRIPTION": "Bücher und Bilder (Catalogue d'exposition, Mayence 1973 et carton d'invitation)",
+    "MATERIALS": "Livre broché (A l'intérieur deux cartons d'invitation pour le vernissage, feuille pliée en 2)",
+    "DIMENSIONS": "23X17X0,5 cm (Catalogue) / 21X14,9 cm (Carton d'invitation)",
+    "AUTHOR": "Barbara Glavert"
+}
+,
+{
+    "ID": "000171",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "2004-22",
+    "CLASSIFICATION": "VIIIE172",
+    "DESCRIPTION": "Europe. Revue littéraire mensuelle (Yvan Goll)",
+    "MATERIALS": "Livre broché",
+    "DIMENSIONS": "0,217X0,13 m",
+    "AUTHOR": "Jean Bertho, Lionel Richard, Eric Robertson, Joseph Delteil, Barbara Glavert-Hesse, Ljubomir Micic, Marc Martin, Catherine Wermester, Albert Ronsin, Alain Virmaux, Henri Béhar, Philippe Brun, Joëlle Gardes Tamine, Pierre Ricas, Danier Grandidier"
+}
+,
+{
+    "ID": "000032",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4059",
+    "CLASSIFICATION": "VIIIA31",
+    "DESCRIPTION": "5 reproductions photo d'un portrait d'Yvan Goll par Robert DELAUNAY, une reproduction extrait d'un livre /5 photo reproductions of  Yvan Goll’s portrait by Robert Delaunay",
+    "MATERIALS": "Photos noir et blanc, papier glacé / Original: dessin au crayon",
+    "DIMENSIONS": "1. 0,178X0,123 m / 2. 0,130X0,86 m / 3. 0,118X0,88 m / 4. 0,114X0,86 m / 5. 0,209X0,132 m",
+    "AUTHOR": "Dessin original: Robert DELAUNAY"
+}
+,
+{
+    "ID": "000131",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4778",
+    "CLASSIFICATION": "VIIIE131",
+    "DESCRIPTION": "Yvan Goll, Claire Goll et leurs amis. Catalogue d'esposition, Strasbourg, Château des Rohan 1961",
+    "MATERIALS": "Livret reuillets réunis par des agrafes",
+    "DIMENSIONS": "20,9X13,5 cm",
+    "AUTHOR": "Musées de Strasboug"
+}
+,
+{
+    "ID": "000131",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "T",
+    "INVENTORY": "4778",
+    "CLASSIFICATION": "VIIIE131",
+    "DESCRIPTION": "Yvan Goll, Claire Goll et leurs amis. Catalogue d'esposition, Strasbourg, Château des Rohan 1961",
+    "MATERIALS": "Livret reuillets réunis par des agrafes",
+    "DIMENSIONS": "20,9X13,5 cm",
+    "AUTHOR": "Musées de Strasboug"
+}
+,
+{
+    "ID": "000028",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4055",
+    "CLASSIFICATION": "VIIIA27",
+    "DESCRIPTION": "Photos reproductions d'un dessin: \"Claire Goll par Albert GLEIZES\" /Photo reproductions of a drawing",
+    "MATERIALS": "Photos  en noir et blanc, papier glacé. Dessin esquisse au crayon gras ou fusain",
+    "DIMENSIONS": "1 et 3: 297X211 mm / 2: 258X204 mm",
+    "AUTHOR": "Esquisse: Albert GLEIZES"
+}
+,
+{
+    "ID": "000030",
+    "LOCATION": "MUSEE PIERRE NOEL",
+    "MEDIA": "P",
+    "INVENTORY": "4057",
+    "CLASSIFICATION": "VIIIA29",
+    "DESCRIPTION": "Reproductions photos d'un dessin: \"Yvan GOLL\" par Albert GLEIZES /Photo reproductions of a drawing : « Yvan Goll » by Albert Gleizes",
+    "MATERIALS": "Photo: noir et blanc, papier glacé / original: dessin au crayon gras ou fusain",
+    "DIMENSIONS": "1: 258X204 mm / 2: 2397X211 mm",
+    "AUTHOR": "Original: Albert GLEIZES"
+}
+];
